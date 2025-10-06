c086155b8d01f0290dc54ac133c3891046027269
dudu <108339131+dudubassetoroch@users.noreply.github.com>
2025-10-06 15:02:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function cad(){
+    let userCad = document.getElementById('userCad').value
+    let keyCad = document.getElementById('keyCad').value
+
+    localStorage.setItem('userCad', userCad)
+    localStorage.setItem('keyCad', keyCad)
+
+    window.location.href = "index.html"
+}
+
+let usersalvo
+let keysalvo
+
+window.onload = function (){
+    usersalvo = localStorage.getItem('userCad')
+    keysalvo = localStorage.getItem('keyCad')
+}
+
+function login(){
+    let user = document.getElementById('user').value
+    let key = document.getElementById('key').value
+
+    let usercorreto = usersalvo
+    let keycorreto = keysalvo
+
+    if(user === usercorreto && key === keycorreto){
+        window.location.href = "home.html"
+    } else {
+        alert("Usuário ou senha incorretos!")
+    }
+}
